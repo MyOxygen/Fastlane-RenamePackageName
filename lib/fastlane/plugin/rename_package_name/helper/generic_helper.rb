23692ce3c3716b2f4ed38ed0edd69479335f66d0
tom-MO <42773461+tom-MO@users.noreply.github.com>
2021-02-04 12:51:51 +0000

    Added method to facilitate appending a directory separator to path
    Includes tests for passing a valid directory. Note: if the path points to a non-existent file without an extension, the method will assume it is a directory.

--- lib/fastlane/plugin/rename_package_name/helper/generic_helper.rb
+++ lib/fastlane/plugin/rename_package_name/helper/generic_helper.rb
@@ -6,4 +6,33 @@ class GenericHelper
   def self.is_nil_or_empty(array)
     return array.nil? || array.length == 0
   end
+
+  def self.append_directory_separator(directory, separator = File::SEPARATOR)
+    # An empty directory or a directory that already ends with the separator
+    # does not need any changes done to it.
+    if is_nil_or_whitespace(directory) || directory.end_with?(separator)
+      return directory
+    end
+
+    # Check that the directory is not a file. If it is, just return itself.
+    # Note that `File.directory?` alone is not enough (see comment below).
+    if File.exist?(directory) && !File.directory?(directory)
+      return directory
+    end
+    
+    # File might not exist. Determine if the path is to a file or a folder.
+    # `File.directory?()` returns false if the directory does not exist, even
+    # if the path is a valid directory path. This is because files don't need
+    # an extension to be a file, so determining whether a non-existent path
+    # is a file or directory is impossible. We can use the path's base name
+    # and check for an extension (has a "."). If the extension exists, the
+    # path points to a file, not a directory. This works for 99% of cases.
+    # Ultimately, if `directory` points to a non-existent file without an
+    # extension, this method will assume that is a directory.
+    if File.basename(directory).include?(".")
+      return directory
+    end
+
+    return directory + separator
+  end
 end
--- spec/generic_helper_spec.rb
+++ spec/generic_helper_spec.rb
@@ -35,4 +35,45 @@ describe GenericHelper do
       end
     end
   end
+
+  context "Append directory separator" do
+    it "Returns itself if nil/whitespace, is not a directory, or already ends with separator" do
+      # Nil or whitespace
+      INVALID_STRINGS.each do |string|
+        expect(GenericHelper.append_directory_separator(string)).to eq(string)
+      end
+
+      # Not a directory
+      file_path = "./spec/static_files/bundle.gradle"
+      expect(GenericHelper.append_directory_separator(file_path)).to eq(file_path)
+
+      # Already ends with separator
+      directory = "./spec/"
+      expect(GenericHelper.append_directory_separator(directory)).to eq(directory)
+    end
+
+    it "Returns existing directory with separator" do
+      directory = "./spec/static_files"
+      expected_directory = "./spec/static_files/"
+      expect(GenericHelper.append_directory_separator(directory)).to eq(expected_directory)
+    end
+
+    it "Returns non-existing directory with separator" do
+      directory = "./spec/random_files"
+      expected_directory = "./spec/random_files/"
+      expect(GenericHelper.append_directory_separator(directory)).to eq(expected_directory)
+    end
+
+    it "Returns path for existing file" do
+      file_path = "./spec/static_files/MainActivity.kt"
+      expected_file_path = "./spec/static_files/MainActivity.kt"
+      expect(GenericHelper.append_directory_separator(file_path)).to eq(expected_file_path)
+    end
+
+    it "Returns path for existing file without extension" do
+      file_path = "./spec/test_files/static_files/Matchfile"
+      expected_file_path = "./spec/test_files/static_files/Matchfile"
+      expect(GenericHelper.append_directory_separator(file_path)).to eq(expected_file_path)
+    end
+  end
 end
