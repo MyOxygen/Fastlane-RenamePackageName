f9e8f9ec499206d719ae4a5afde00171c00478cf
tom-MO <42773461+tom-MO@users.noreply.github.com>
2021-02-09 16:57:13 +0000

    Created example Flutter app

--- example/app/src/main/java/com/example/myapp/MainApplication.java
+++
@@ -1,6 +0,0 @@
-package com.example.myapp;
-
-import android.app.Application;
-
-public class MainApplication extends Application {
-}
--- example/app/src/main/kotlin/com/example/myapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapp
-
-import androidx.appcompat.app.AppCompatActivity
-
-class MainActivity : AppCompatActivity() {
-}
